9268e0a6d2b7cd3a3c793c040f9b498cb7b52476
Moises <sorianoguerrero89@gmail.com>
2018-09-07 10:51:41 -0600

    first commit

---
+++ RiderApp/app/src/main/java/com/example/admin/rideapp/RideActivity.kt
@@ -0,0 +1,12 @@
+package com.example.admin.rideapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RideActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ride)
+    }
+}
---
+++ RiderApp/app/src/main/java/com/example/admin/rideapp/base/BaseViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.admin.rideapp.base
+
+import android.arch.lifecycle.ViewModel
+
+abstract class BaseViewModel : ViewModel(){
+
+}
